e04741c6c5048df8ebb6b5f85d7e816a224d6af7
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-14 13:24:33 -0300

    feat: implements GET users route

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\UserController;
 */
 
 Route::post('/users', [UserController::class, 'store']);
+Route::get('/users', [UserController::class, 'index']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
