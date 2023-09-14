9c4bd96118b9851d09062d49532ab2c401fbbf22
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-14 17:12:51 -0300

    feat: implements DELETE users/{id} route

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::post('/users', [UserController::class, 'store']);
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{id}', [UserController::class, 'show']);
 Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
