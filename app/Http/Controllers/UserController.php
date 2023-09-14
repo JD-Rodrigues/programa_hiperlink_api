b66843d18b4dee1a30d0acbab4dac0f8f3b8d434
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-14 11:29:45 -0300

    feat: implements UserController->store() method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -18,8 +19,36 @@ class UserController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
+    {   
+        $rules = [
+            'name'=>'required|string|max:255|',
+            'email'=>'required|email|max:255',
+            'password'=>'required|string|max:255',
+            'authorize_location'=>'required|boolean'
+        ];
+
+        $validationFailMessages = [
+            'name.required'=>'Insira o nome do usuário.',
+            'name.max'=>'O nome não pode ter mais de 255 caracteres.',
+            'email.required'=>'Insira o e-mail do usuário.',
+            'email.email'=>'O email precisa ter o formato de email. Ex.: seunome@hotmail.com.',
+            'email.max'=>'O email não pode ter mais de 255 caracteres.',
+            'password.required'=>'Insira a senha do usuário.',
+            'password.max'=>'A senha não pode ter mais de 255 caracteres.',
+            'authorize_location.required'=>'Informe se autoriza a localização.',
+            'authorize_location.boolean'=>'O campo de "authorize_location" só aceita 1 para "true" ou 0 para "false".'
+        ];
+
+
+        try {
+            $request->validate($rules, $validationFailMessages);
+
+            return User::create($request->only('name',  'email', 'password', 'authorize_location'));
+        } catch (\Throwable $e) {
+            return $e->getMessage();
+        }
+
+
     }
 
     /**
