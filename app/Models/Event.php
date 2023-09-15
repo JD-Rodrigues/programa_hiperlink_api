f1cbeb1af9d7a63406023e7fcc4ee1c2b852a90e
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-15 16:50:44 -0300

    feat: implements Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Event extends Model
 {
-    use HasFactory;
+    use HasFactory, softDeletes;
+
+   protected $fillable = ['title', 'image', 'start_date'];
 }
