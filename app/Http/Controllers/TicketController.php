409e41523b8a7b687956b9b83d8a9ad7355265d3
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-15 22:16:05 -0300

    feat: implements TicketController index()and store()  methods

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,29 +3,67 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ticket;
+use App\Models\User;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
+{ 
     public function index()
-    {
-        //
+    {      
+
+        $tickets = Ticket::join('users', 'tickets.id_user', '=', 'users.id')
+            ->join('events', 'tickets.id_event', '=', 'events.id')
+            ->select(
+                'users.name as nome_usuario',
+                'events.title as titulo_evento',
+                'events.start_date as data_do_evento',
+                'tickets.created_at as data_da_compra'
+            )
+            ->get();
+
+        return $tickets;
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'id_user'=>'required|integer',
+            'id_event'=>'required|integer'
+        ];
+
+        $validateFailMessages = [
+            'id_user.required'=>'Insira o valor para o id_user.',
+            'id_user.integer'=>'O valor para o id_user deve ser um número inteiro.',
+            'id_event.required'=>'Insira o valor para o id_event.',
+            'id_event.integer'=>'O valor para o id_event deve ser um número inteiro.',
+        ];
+
+        try {
+            $request->validate($rules, $validateFailMessages);
+
+            if(!User::find($request->input('id_user'))){
+                return "Usuário não encontrado";
+            }
+    
+            if(!Event::find($request->input('id_event'))){
+                return "Evento não encontrado";
+            }
+
+            return [
+                'Ingresso adquirido:'=> Ticket::create($request->only(['id_user', 'id_event']))
+            ];
+
+        } catch (\Throwable $e) {
+            return $e->getMessage();
+        }
+
+
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Ticket $ticket)
     {
         //
