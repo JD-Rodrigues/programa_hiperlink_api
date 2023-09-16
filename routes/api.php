afb30dffc5ee205712cd847f515d68fefb682e62
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-15 22:17:26 -0300

    refactor: changes routes to individual, instead using apiResource

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\TicketController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,18 +17,20 @@ use App\Http\Controllers\EventController;
 |
 */
 
-// Route::post('/users', [UserController::class, 'store']);
-// Route::get('/users', [UserController::class, 'index']);
-// Route::get('/users/{id}', [UserController::class, 'show']);
-// Route::put('/users/{id}', [UserController::class, 'update']);
-// Route::delete('/users/{id}', [UserController::class, 'destroy']);
-Route::apiResource('users', UserController::class);
-Route::apiResource('events', EventController::class);
 Route::post('/login', [UserController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function(){
-    Route::post('/logout', [UserController::class, 'logout']);
-});
+Route::post('/users', [UserController::class, 'store']);
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::post('/events', [EventController::class, 'store']);
+    Route::get('/events', [EventController::class, 'index']);
+    Route::get('/events/{id}', [EventController::class, 'show']);
+    Route::put('/events/{id}', [EventController::class, 'update']);
+    Route::delete('/events/{id}', [EventController::class, 'destroy']);
+    Route::post('/tickets', [TicketController::class, 'store']);
+    Route::get('/tickets', [TicketController::class, 'index']);
+});
